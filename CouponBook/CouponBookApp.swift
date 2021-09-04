54694eb32bc19ea58444b735dfdf298d0d638f08
Kipland Melton <kiplandmelton@gmail.com>
2021-09-03 20:52:28 -0500

    Initial Commit

---
+++ CouponBook/CouponBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CouponBookApp.swift
+//  CouponBook
+//
+//  Created by Kipland Melton on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CouponBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
